8156115a7084a636d4866be1a5c805ca9c67f0c9
MioraRatsiresy <miora.ratsiresy@gmail.com>
2023-02-03 12:09:39 +0300

    Update Photo.tsx

--- src/pages/Photo.tsx
+++ src/pages/Photo.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { isPlatform } from '@ionic/react';
 
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
